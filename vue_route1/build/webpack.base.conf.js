46fada8eb6bf620ceb30ee8a5659f4aec1e1e74c
hanxiao2100 <hanxiao2100@qq.com>
2018-12-04 11:11:58 +0800

    vue-route1 vue-load 升级为v15

--- vue_route1/build/webpack.base.conf.js
+++ vue_route1/build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -11,6 +12,9 @@ function resolve (dir) {
 
 
 module.exports = {
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
