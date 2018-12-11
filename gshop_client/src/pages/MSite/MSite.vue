1d3310455d6a2c17bcd84bacf327f096799e70f8
hanxiao <hanxiao2100@qq.com>
2018-12-12 01:08:23 +0800

    目录设计

---
+++ gshop_client/src/pages/MSite/MSite.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>MSite</div>
+</template>
+
+<script>
+export default {
+  name: 'MSite'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ gshop_client/src/pages/Seaarch/Seaarch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Seaarch</div>
+</template>
+
+<script>
+export default {
+  name: 'Seaarch'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
