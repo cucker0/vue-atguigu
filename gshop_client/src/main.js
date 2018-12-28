64967b6f3d0eb39f9082f049a2ebe8e6697037c1
hanxiao <hanxiao2100@qq.com>
2018-12-29 00:15:57 +0800

    gshop_client main.js 引入mock 修复eslink格式要求

--- gshop_client/src/main.js
+++ gshop_client/src/main.js
@@ -7,9 +7,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 import { Button } from 'mint-ui'
+import './mock/mockServer' // 引入mockServer
 
 Vue.component(Button.name, Button) // 注册全局的 <mt-button> 组件标签
-import './mock/mockServer' // 引入mockServer
 
 /* eslint-disable no-new */
 new Vue({
